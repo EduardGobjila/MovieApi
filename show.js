b925ab0f4bd9dc16903ff10bda9c65efd83b1ad4
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-28 13:40:13 +0100

    added back the js for the show page

--- show.js
+++ show.js
@@ -1,36 +1,34 @@
+const queryStrings = window.location.search
+const urlParams = new URLSearchParams(queryStrings);
+const id = urlParams.get('movie')
+const ul = document.querySelector('#ul')
+const header = document.querySelector('#header')
+const descript = document.querySelector('#description')
+
+// console.log(ul)
 
 
-// const queryStrings = window.location.search
-// const urlParams = new URLSearchParams(queryStrings);
-// const id = urlParams.get('movie')
-// const ul = document.querySelector('#ul')
-// const header = document.querySelector('#header')
-// const descript = document.querySelector('#description')
+const showPage = (id) => {
+  fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data)
+      const show = `
+        <img src="${data.results[0].image}">
+      `;
 
-// // console.log(ul)
+      const title = `
+        <h1>${data.results[0].title}</h1>
+      `;
 
-
-// const showPage = (id) => {
-//   fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
-//     .then(response => response.json())
-//     .then(data => {
-//       // console.log(data)
-//       const show = `
-//         <img src="${data.results[0].image}">
-//       `;
-
-//       const title = `
-//         <h1>${data.results[0].title}</h1>
-//       `;
-
-//       const desc = `
-//       <h3>${data.results[0].description}</h3>
-//       `
-//       ul.insertAdjacentHTML("beforeend", show)
-//       header.insertAdjacentHTML('beforeend', title)
-//       descript.insertAdjacentHTML('beforeend', desc)
-//     })
-// }
+      const desc = `
+      <h3>${data.results[0].description}</h3>
+      `
+      ul.insertAdjacentHTML("beforeend", show)
+      header.insertAdjacentHTML('beforeend', title)
+      descript.insertAdjacentHTML('beforeend', desc)
+    })
+}
 
 
-// showPage(id);
+showPage(id);

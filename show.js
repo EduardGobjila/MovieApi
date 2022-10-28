67e6849271a625a9510b20d7ce61573067029275
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-28 14:27:08 +0100

    added a loader

--- show.js
+++ show.js
@@ -5,30 +5,41 @@ const image = document.querySelector('#image')
 const header = document.querySelector('#header')
 const descript = document.querySelector('#description')
 
+// console.log(loader)
 // console.log(ul)
 
 
-const showPage = (id) => {
-  fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
-    .then(response => response.json())
-    .then(data => {
-      // console.log(data)
-      const show = `
-        <img src="${data.results[0].image}">
-      `;
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader')
 
-      const title = `
-        <h1>${data.results[0].title}</h1>
-      `;
+  loader.classList.add('loader-hidden')
 
-      const desc = `
-      <h3>${data.results[0].description}</h3>
-      `
-      image.insertAdjacentHTML("beforeend", show)
-      header.insertAdjacentHTML('beforeend', title)
-      descript.insertAdjacentHTML('beforeend', desc)
-    })
-}
+})
 
 
-showPage(id);
+
+
+  const showPage = (id) => {
+    fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+        const show = `
+          <img src="${data.results[0].image}">
+        `;
+
+        const title = `
+          <h1>${data.results[0].title}</h1>
+        `;
+
+        const desc = `
+        <h3>${data.results[0].description}</h3>
+        `
+        image.insertAdjacentHTML("beforeend", show)
+        header.insertAdjacentHTML('beforeend', title)
+        descript.insertAdjacentHTML('beforeend', desc)
+      })
+
+
+      showPage(id);
+  }

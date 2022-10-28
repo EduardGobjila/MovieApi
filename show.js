2e1eb0ab8bb09d9b3f61bdbc92eb334693d029e0
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-28 12:56:48 +0100

    finished fetching data logic

--- show.js
+++ show.js
@@ -4,6 +4,8 @@ const queryStrings = window.location.search
 const urlParams = new URLSearchParams(queryStrings);
 const id = urlParams.get('movie')
 const ul = document.querySelector('#ul')
+const header = document.querySelector('#header')
+const descript = document.querySelector('#description')
 
 // console.log(ul)
 
@@ -12,11 +14,21 @@ const showPage = (id) => {
   fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      // console.log(data)
       const show = `
-        ${data.results[0].image}
+        <img src="${data.results[0].image}">
       `;
+
+      const title = `
+        <h1>${data.results[0].title}</h1>
+      `;
+
+      const desc = `
+      <h3>${data.results[0].description}</h3>
+      `
       ul.insertAdjacentHTML("beforeend", show)
+      header.insertAdjacentHTML('beforeend', title)
+      descript.insertAdjacentHTML('beforeend', desc)
     })
 }
 

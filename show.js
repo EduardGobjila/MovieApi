16aa28c019a964f4c95d1561623d434f4789e54e
Eduard Gobjila <edikgobjila01@gmail.com>
2022-10-29 15:23:23 +0100

    added timeout for the loader

--- show.js
+++ show.js
@@ -9,37 +9,43 @@ const descript = document.querySelector('#description')
 // console.log(ul)
 
 
+
+
+
+const showPage = (id) => {
+  fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data)
+      const show = `
+        <img src="${data.results[0].image}">
+      `;
+
+      const title = `
+        <h1>${data.results[0].title}</h1>
+      `;
+
+      const desc = `
+      <h3>${data.results[0].description}</h3>
+      `
+      image.insertAdjacentHTML("beforeend", show)
+      header.insertAdjacentHTML('beforeend', title)
+      descript.insertAdjacentHTML('beforeend', desc)
+    })
+
+
+  }
+
+
+
+
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loader')
 
-  loader.classList.add('loader-hidden')
+  showPage(id);
+ setTimeout(() => {
+
+   loader.classList.add('loader-hidden')
+ }, 3000);
 
 })
-
-
-
-
-  const showPage = (id) => {
-    fetch(`https://imdb-api.com/en/API/SearchMovie/k_ns10ugu1/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        // console.log(data)
-        const show = `
-          <img src="${data.results[0].image}">
-        `;
-
-        const title = `
-          <h1>${data.results[0].title}</h1>
-        `;
-
-        const desc = `
-        <h3>${data.results[0].description}</h3>
-        `
-        image.insertAdjacentHTML("beforeend", show)
-        header.insertAdjacentHTML('beforeend', title)
-        descript.insertAdjacentHTML('beforeend', desc)
-      })
-
-
-      showPage(id);
-  }
